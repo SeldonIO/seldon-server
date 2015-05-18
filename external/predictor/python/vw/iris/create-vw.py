452437160815a4f6f528f0de8adc78e11d1cf823
Clive Cox <cc@seldon.io>
2015-05-18 21:06:06 +0100

    handle empty lines in vw train for iris data

--- external/predictor/python/vw/iris/create-vw.py
+++ external/predictor/python/vw/iris/create-vw.py
@@ -2,13 +2,15 @@ import sys
 
 for line in sys.stdin:
     line = line.rstrip()
-    (f1,f2,f3,f4,cl) = line.split(',')
-    if cl == "Iris-setosa":
-        print "1 ",
-    elif cl == "Iris-versicolor":
-        print "2 ",
-    else:
-        print "3 ",
-    print "|f",
-    print " f1:"+f1+" f2:"+f2+" f3:"+f3+" f4:"+f4
-
+    try:
+        (f1,f2,f3,f4,cl) = line.split(',')
+        if cl == "Iris-setosa":
+            print "1 ",
+        elif cl == "Iris-versicolor":
+            print "2 ",
+        else:
+            print "3 ",
+        print "|f",
+        print " f1:"+f1+" f2:"+f2+" f3:"+f3+" f4:"+f4
+    except:
+        continue
