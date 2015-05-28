ca1209a72185b8a07a177789096b1bb01d4038dd
Gurminder Sunner <gsunner2000@gmail.com>
2015-05-28 15:40:41 +0100

    fix for LINUX_IMAGE_EXTRA_PACKAGE

--- vm/build/install-deps/install-linux-image-extra.sh
+++ vm/build/install-deps/install-linux-image-extra.sh
@@ -8,5 +8,6 @@ STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 export DEBIAN_FRONTEND=noninteractive
 
 # Ensure docker will use aufs
+LINUX_IMAGE_EXTRA_PACKAGE=linux-image-extra-$(uname -r)
 sudo apt-get install -y -q $LINUX_IMAGE_EXTRA_PACKAGE
 
