2827e681f48d5e9d9c0cf3eedf7aa0540fbb50ac
Clive Cox <cc@seldon.io>
2015-05-29 12:27:25 +0100

    fix typos in mf job

--- offline-jobs/spark/src/main/scala/io/seldon/spark/mllib/MfModelCreation.scala
+++ offline-jobs/spark/src/main/scala/io/seldon/spark/mllib/MfModelCreation.scala
@@ -166,11 +166,10 @@ class MfModelCreation(private val sc : SparkContext,config : MfConfig) {
     actionsByScore.take(10).foreach(println)
     val itemsByCount: RDD[(Int, Int)] = actionsByScore.map(x => (x._1._1, 1)).reduceByKey(_ + _)
     val itemsCount = itemsByCount.count()
-    val topQuarter = (itemsCount * 1).toInt
     println("total actions " + actions.count())
-    println("total stories " + itemsByCount.count())
+    println("total items " + itemsByCount.count())
 
-    val usersByCount = actionsByScore.map(x=>(x._2,1)).reduceByKey(_+_)
+    val usersByCount = actionsByScore.map(x=>(x._1._2,1)).reduceByKey(_+_)
     val usersCount = usersByCount.count()
     println("total users " + usersCount)
 
