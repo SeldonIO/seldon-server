fcc70efb5fe12bdbedae20d3edf3f3fc15beb29a
Gurminder Sunner <gsunner2000@gmail.com>
2015-05-30 09:05:44 +0100

    project deps added

--- vm/build/vagrant/ubuntu-trusty/install-deps.sh
+++ vm/build/vagrant/ubuntu-trusty/install-deps.sh
@@ -22,20 +22,30 @@ export INSTALL_DEPS_SPARK_HOME=/home/vagrant/apps/spark
 export INSTALL_DEPS_SELDON_SERVER_HOME=/home/vagrant/seldon-server
 export INSTALL_DEPS_VM_USER=vagrant
 
-##${INSTALL_DEPS_DIR}/install-locale.sh
-##${INSTALL_DEPS_DIR}/install-linux-image-extra.sh
-#${INSTALL_DEPS_DIR}/install-docker.sh
-#${INSTALL_DEPS_DIR}/install-vim.sh
-#${INSTALL_DEPS_DIR}/install-jdk7.sh
-#${INSTALL_DEPS_DIR}/install-tomcat.sh
-#${INSTALL_DEPS_DIR}/install-spark.sh
-#${INSTALL_DEPS_DIR}/install-bash-profile.sh
-#${INSTALL_DEPS_DIR}/install-python-deps.sh
-#${INSTALL_DEPS_DIR}/install-misc.sh
+###############################################################################
+# Clone the "seldon-server" project
+(cd ~/ && git clone https://github.com/SeldonIO/seldon-server.git)
+###############################################################################
 
-#use_pre_built_projects
-#${INSTALL_DEPS_DIR}/install-seldon-api-webapps.sh
+###############################################################################
+# Clone the "movie-demo-setup" project
+(cd ~/ && git clone https://github.com/SeldonIO/movie-demo-setup)
+###############################################################################
 
-#${INSTALL_DEPS_DIR}/install-bower-interaction-fix.sh
-#${INSTALL_DEPS_DIR}/install-movie-demo-build-deps.sh
+${INSTALL_DEPS_DIR}/install-locale.sh
+${INSTALL_DEPS_DIR}/install-linux-image-extra.sh
+${INSTALL_DEPS_DIR}/install-docker.sh
+${INSTALL_DEPS_DIR}/install-vim.sh
+${INSTALL_DEPS_DIR}/install-jdk7.sh
+${INSTALL_DEPS_DIR}/install-tomcat.sh
+${INSTALL_DEPS_DIR}/install-spark.sh
+${INSTALL_DEPS_DIR}/install-bash-profile.sh
+${INSTALL_DEPS_DIR}/install-python-deps.sh
+${INSTALL_DEPS_DIR}/install-misc.sh
+
+use_pre_built_projects
+${INSTALL_DEPS_DIR}/install-seldon-api-webapps.sh
+
+${INSTALL_DEPS_DIR}/install-bower-interaction-fix.sh
+${INSTALL_DEPS_DIR}/install-movie-demo-build-deps.sh
 
