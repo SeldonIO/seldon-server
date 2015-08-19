14680a36d4ce15fef0bd3db34f1a94bd35e4227f
Clive Cox <cc@seldon.io>
2015-08-19 10:21:42 +0100

    skeleton for process events job

---
+++ offline-jobs/spark/src/main/scala/io/seldon/spark/events/ProcessEventsJob.scala
@@ -0,0 +1,163 @@
+package io.seldon.spark.events
+
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import io.seldon.spark.SparkUtils
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.feature.IDF
+import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
+import java.sql.ResultSet
+import scala.collection.mutable.ListBuffer
+import org.apache.spark.mllib.feature.IDF
+
+case class ProcessEventsConfig(
+    client : String = "",
+    inputPath : String = "/seldon-models",
+    outputPath : String = "/seldon-models",
+    startDay : Int = 1,
+    days : Int = 1,
+    awsKey : String = "",
+    awsSecret : String = "",
+    local : Boolean = false,
+    zkHosts : String = ""
+    
+   )
+
+class ProcessEventsJob(private val sc : SparkContext,config : ProcessEventsConfig) {
+
+  
+  def parseJson(path : String) = {
+    
+    val rdd = sc.textFile(path).map{line =>
+      import org.json4s._
+      import org.json4s.jackson.JsonMethods._
+      implicit val formats = DefaultFormats
+    
+      val json = parse(line)
+      val client = (json \ "client").extract[String]
+
+       (client,line)
+      
+      }
+    
+    rdd
+  }
+  
+  
+  
+  def run()
+  {
+    val fileGlob = config.inputPath + "/" + config.client+"/actions/"+SparkUtils.getS3UnixGlob(config.startDay,config.days)+"/*"
+   
+    val jsonRdd = parseJson(fileGlob)
+
+    /*
+     * Do stuff
+     */
+    
+  }
+}
+
+ object ProcessEventsJob
+{
+    def updateConf(config : ProcessEventsConfig) =
+  {
+    import io.seldon.spark.zookeeper.ZkCuratorHandler
+    var c = config.copy()
+    if (config.zkHosts.nonEmpty) 
+     {
+       val curator = new ZkCuratorHandler(config.zkHosts)
+       val path = "/all_clients/"+config.client+"/offline/tagaffinity"
+       if (curator.getCurator.checkExists().forPath(path) != null)
+       {
+         val bytes = curator.getCurator.getData().forPath(path)
+         val j = new String(bytes,"UTF-8")
+         println("Confguration from zookeeper -> "+j)
+         import org.json4s._
+         import org.json4s.jackson.JsonMethods._
+         implicit val formats = DefaultFormats
+         val json = parse(j)
+         import org.json4s.JsonDSL._
+         import org.json4s.jackson.Serialization.write
+         type DslConversion = ProcessEventsConfig => JValue
+         val existingConf = write(c) // turn existing conf into json
+         val existingParsed = parse(existingConf) // parse it back into json4s internal format
+         val combined = existingParsed merge json // merge with zookeeper value
+         c = combined.extract[ProcessEventsConfig] // extract case class from merged json
+         c
+       }
+       else 
+       {
+           println("Warning: using default configuaration - path["+path+"] not found!");
+           c
+       }
+     }
+     else 
+     {
+       println("Warning: using default configuration - no zkHost!");
+       c
+     }
+  }
+  
+   def main(args: Array[String]) 
+  {
+
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
+
+    var c = new ProcessEventsConfig()
+    val parser = new scopt.OptionParser[Unit]("ProcessEventsJob") {
+    head("ProcessEventsJob", "1.0")
+       opt[Unit]('l', "local") foreach { x => c = c.copy(local = true) } text("local mode - use local Master")
+        opt[String]('c', "client") required() valueName("<client>") foreach { x => c = c.copy(client = x) } text("client name (will be used as db and folder suffix)")
+        opt[String]('i', "inputPath") valueName("path url") foreach { x => c = c.copy(inputPath = x) } text("path prefix for input")
+        opt[String]('o', "outputPath") valueName("path url") foreach { x => c = c.copy(outputPath = x) } text("path prefix for output")
+        opt[Int]('r', "days") foreach { x =>c = c.copy(days = x) } text("number of days in past to get foreachs for")
+        opt[Int]("startDay") foreach { x =>c = c.copy(startDay = x) } text("start day in unix time")
+        opt[String]('a', "awskey") valueName("aws access key") foreach { x => c = c.copy(awsKey = x) } text("aws key")
+        opt[String]('s', "awssecret") valueName("aws secret") foreach { x => c = c.copy(awsSecret = x) } text("aws secret")
+        opt[String]('z', "zookeeper") valueName("zookeeper hosts") foreach { x => c = c.copy(zkHosts = x) } text("zookeeper hosts (comma separated)")        
+        
+
+    }
+    
+      if (parser.parse(args)) // Parse to check and get zookeeper if there
+    {
+      c = updateConf(c) // update from zookeeper args
+      parser.parse(args) // overrride with args that were on command line
+
+       val conf = new SparkConf().setAppName("ProcessEventsJob")
+
+      if (c.local)
+        conf.setMaster("local")
+ //       .set("spark.akka.frameSize", "300")
+
+      val sc = new SparkContext(conf)
+      try
+      {
+        sc.hadoopConfiguration.set("fs.s3.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+        if (c.awsKey.nonEmpty && c.awsSecret.nonEmpty)
+        {
+         sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", c.awsKey)
+         sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", c.awsSecret)
+        }
+        println(c)
+        val cu = new ProcessEventsJob(sc,c)
+        cu.run()
+      }
+      finally
+      {
+        println("Shutting down job")
+        sc.stop()
+      }
+   } 
+   else 
+   {
+      
+   }
+  }
+}
+ 
