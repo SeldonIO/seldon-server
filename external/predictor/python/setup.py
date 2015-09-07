3de886eb4b153111b99d28d8e272ffae0ce25265
Clive Cox <cc@seldon.io>
2015-09-07 08:23:01 +0100

    update docker python versions

--- external/predictor/python/setup.py
+++ external/predictor/python/setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='seldon',
-      version='1.1',
+      version='1.1.1',
       description='Seldon Python Utilities',
       author='Clive Cox',
       author_email='support@seldon.io',
