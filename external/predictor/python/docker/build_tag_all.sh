9c7e44cbac609835128d939f2b8dbf71c931d682
Clive Cox <cc@seldon.io>
2015-10-30 15:28:43 +0000

    fix build all docker containers

--- external/predictor/python/docker/build_tag_all.sh
+++ external/predictor/python/docker/build_tag_all.sh
@@ -4,5 +4,5 @@ pushd vw_train ; make build ; make tag ; popd
 pushd vw_runtime ; make build ; make tag ; popd
 pushd xgboost_train ; make build ; make tag ; popd
 pushd xgboost_runtime ; make build ; make tag ; popd
-pushd examples/iris ; make build ; make tag ; popd
+pushd examples/iris ; make build_pipeline ; make tag ; popd
 
