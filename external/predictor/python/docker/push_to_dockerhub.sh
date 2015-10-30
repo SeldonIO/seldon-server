11b1db2c3ba06443c1be94d5c64a83a657220bef
Clive Cox <cc@seldon.io>
2015-10-30 18:54:59 +0000

    push to docker hub script

--- external/predictor/python/docker/push_to_dockerhub.sh
+++ external/predictor/python/docker/push_to_dockerhub.sh
@@ -1,5 +1,3 @@
 pushd pipelines ; make push_to_dockerhub ; popd
-pushd vw_train ; make push_to_dockerhub ; popd
-pushd vw_runtime ; make push_to_dockerhub ; popd
-pushd xgboost_train ; make push_to_dockerhub ; popd
-pushd xgboost_runtime ; make push_to_dockerhub ; popd
+pushd examples/iris ; make push_to_dockerhub ; popd
+
