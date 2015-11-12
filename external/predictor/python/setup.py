8f157f7b6c6e1d199cb128db13b4efd4991a8134
Clive Cox <cc@seldon.io>
2015-11-12 10:32:33 +0000

    update python setup.py dependency fro wabbit-wappa

--- external/predictor/python/setup.py
+++ external/predictor/python/setup.py
@@ -3,12 +3,15 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='seldon',
-      version='1.2',
+      version='1.2.1',
       description='Seldon Python Utilities',
       author='Clive Cox',
       author_email='support@seldon.io',
       url='https://github.com/SeldonIO/seldon-server',
       license='Apache 2 License',
-      install_requires=['wabbit_wappa','xgboost','kazoo','sklearn','scipy','numpy','boto','filechunkio'],
+      install_requires=['wabbit_wappa==3.0.2','xgboost','kazoo','sklearn','scipy','numpy','boto','filechunkio','keras'],
+      dependency_links = [
+          'https://github.com/SeldonIO/wabbit_wappa/archive/3.0.2.zip#egg=wabbit-wappa-3.0.2'
+      ],
       packages=['seldon', 'seldon.pipeline'],
       )
