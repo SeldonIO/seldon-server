7ca255b92015c884140fd8a143fbb7c7a558902d
Gurminder Sunner <gsunner2000@gmail.com>
2015-11-16 17:40:48 +0000

    python deps for vm changed to use anaconda

--- vm/build/install-deps/install-python-deps.sh
+++ vm/build/install-deps/install-python-deps.sh
@@ -7,8 +7,22 @@ STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get install -y python-pip make python-dev libmysqlclient-dev mysql-client-core-5.6
-sudo pip install unicodecsv
-sudo pip install MySQL-python
-sudo pip install kazoo
+ANACONDA_INSTALL_FILE=Anaconda2-2.4.0-Linux-x86_64.sh
+wget -O /tmp/${ANACONDA_INSTALL_FILE} http://static.seldon.io/vm-resources/anaconda/${ANACONDA_INSTALL_FILE}
+
+bash /tmp/Anaconda2-2.4.0-Linux-x86_64.sh -b
+
+# use anaconda for rest of python install
+export PATH=/home/ubuntu/anaconda2/bin:${PATH}
+
+sudo apt-get update
+sudo apt-get install -y make libmysqlclient-dev mysql-client-core-5.6
+pip install unicodecsv
+pip install MySQL-python
+pip install kazoo
+
+sudo apt-get install -y git
+sudo apt-get install -y g++
+pip install -e git+git://github.com/SeldonIO/wabbit_wappa#egg=wabbit-wappa-3.0.2
+pip install seldon
 
