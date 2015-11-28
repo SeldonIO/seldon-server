d9a4c2b1fdb32e06b2bdc1602afa982240d3685a
Clive Cox <cc@seldon.io>
2015-11-28 17:12:47 +0000

    update python setup.py

--- python/setup.py
+++ python/setup.py
@@ -3,13 +3,13 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='seldon',
-      version='1.2.2',
+      version='1.3',
       description='Seldon Python Utilities',
       author='Clive Cox',
       author_email='support@seldon.io',
       url='https://github.com/SeldonIO/seldon-server',
       license='Apache 2 License',
-      install_requires=['wabbit_wappa==3.0.2','xgboost','kazoo','sklearn','scipy','numpy','boto','filechunkio','keras'],
+      install_requires=['wabbit_wappa==3.0.2','xgboost','kazoo','sklearn','scipy','numpy','boto','filechunkio','keras', 'pylibmc'],
       dependency_links = [
           'https://github.com/SeldonIO/wabbit_wappa/archive/3.0.2.zip#egg=wabbit-wappa-3.0.2'
       ],
