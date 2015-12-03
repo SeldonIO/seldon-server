7485779a5495f0bc8a305def3d637aabd61912c2
Gurminder Sunner <gsunner2000@gmail.com>
2015-12-03 16:16:25 +0000

    dependencies for seldon shell updated

--- python/setup.py
+++ python/setup.py
@@ -9,9 +9,12 @@ setup(name='seldon',
       author_email='support@seldon.io',
       url='https://github.com/SeldonIO/seldon-server',
       license='Apache 2 License',
-      install_requires=['wabbit_wappa==3.0.2','xgboost','kazoo','scikit-learn>=0.17','scipy','numpy','boto','filechunkio','keras', 'pylibmc', 'annoy', 'gensim', "Flask", "pandas>=0.17"],
+      install_requires=['wabbit_wappa==3.0.2','xgboost','kazoo','scikit-learn>=0.17','scipy','numpy','boto','filechunkio','keras', 'pylibmc', 'annoy', 'gensim', "Flask", "pandas>=0.17", "cmd2", "MySQL-python"],
       dependency_links = [
           'https://github.com/SeldonIO/wabbit_wappa/archive/3.0.2.zip#egg=wabbit-wappa-3.0.2'
       ],
-      packages=['seldon', 'seldon.pipeline', 'seldon.microservice', 'seldon.text'],
+      packages=['seldon', 'seldon.pipeline', 'seldon.microservice', 'seldon.text', 'seldon.shell'],
+      package_dir={'seldon.shell': 'seldon/shell'},
+      package_data={'seldon.shell': ['dbschema/mysql/api.sql', 'dbschema/mysql/client.sql']},
+      scripts=['bin/seldon'],
       )
