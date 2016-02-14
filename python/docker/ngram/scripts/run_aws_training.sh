79bd6f4de655c6493c92f9f67c589c538cf3affe
Clive Cox <cc@seldon.io>
2016-02-14 16:36:54 +0000

    add memory argument to ngram training

--- python/docker/ngram/scripts/run_aws_training.sh
+++ python/docker/ngram/scripts/run_aws_training.sh
@@ -18,5 +18,5 @@ AWS_KEY=$5
 AWS_SECRET=$6
 
 python create_corpus.py --client ${CLIENT} --bucket ${BUCKET} --aws_key ${AWS_KEY} --aws_secret ${AWS_SECRET} --startDay ${START_DAY} --numDays ${DAYS}  --corpus data.corpus
-/kenlm/bin/lmplz -o 3 --prune 0 1 1  < data.corpus > model.arpa
+/kenlm/bin/lmplz -o 3 --prune 0 1 1 -S 60%  < data.corpus > model.arpa
 python build_recommender.py --aws_key ${AWS_KEY} --aws_secret ${AWS_SECRET} --arpa model.arpa --dst s3://${BUCKET}/${CLIENT}/ngram/${START_DAY}
