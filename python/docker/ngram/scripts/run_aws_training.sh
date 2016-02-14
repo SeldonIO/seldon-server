b509ae36685e20d115bfee4576aafaa14d2b1011
Clive Cox <cc@seldon.io>
2016-02-14 20:52:04 +0000

    update aws ngram run

--- python/docker/ngram/scripts/run_aws_training.sh
+++ python/docker/ngram/scripts/run_aws_training.sh
@@ -5,19 +5,29 @@ set -o errexit
 
 STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ "$#" -ne 7 ]; then
-  echo "Usage: $0 CLIENT BUCKET START_DAY DAYS AWS_KEY AWS_SECRET MEM_PERCENT" >&2
-  exit 1
+if [ "$#" -eq 7 ]; then
+    CLIENT=$1
+    BUCKET=$2
+    START_DAY=$3
+    DAYS=$4
+    AWS_KEY=$5
+    AWS_SECRET=$6
+    MEM_PERCENTAGE=$7
+
+    python create_corpus.py --client ${CLIENT} --bucket ${BUCKET} --aws_key ${AWS_KEY} --aws_secret ${AWS_SECRET} --startDay ${START_DAY} --numDays ${DAYS}  --corpus data.corpus
+    /kenlm/bin/lmplz -o 3 --prune 0 1 1 -S ${MEM_PERCENTAGE}%  < data.corpus > model.arpa
+    python build_recommender.py --aws_key ${AWS_KEY} --aws_secret ${AWS_SECRET} --arpa model.arpa --dst s3://${BUCKET}/${CLIENT}/ngram/${START_DAY}
 fi
 
-CLIENT=$1
-BUCKET=$2
-START_DAY=$3
-DAYS=$4
-AWS_KEY=$5
-AWS_SECRET=$6
-MEM_PERCENTAGE=$7
+if [ "$#" -eq 5 ]; then
+    CLIENT=$1
+    BUCKET=$2
+    START_DAY=$3
+    DAYS=$4
+    MEM_PERCENTAGE=$5
 
-python create_corpus.py --client ${CLIENT} --bucket ${BUCKET} --aws_key ${AWS_KEY} --aws_secret ${AWS_SECRET} --startDay ${START_DAY} --numDays ${DAYS}  --corpus data.corpus
-/kenlm/bin/lmplz -o 3 --prune 0 1 1 -S ${MEM_PERCENTAGE}%  < data.corpus > model.arpa
-python build_recommender.py --aws_key ${AWS_KEY} --aws_secret ${AWS_SECRET} --arpa model.arpa --dst s3://${BUCKET}/${CLIENT}/ngram/${START_DAY}
+    python create_corpus.py --client ${CLIENT} --bucket ${BUCKET} --startDay ${START_DAY} --numDays ${DAYS}  --corpus data.corpus
+    /kenlm/bin/lmplz -o 3 --prune 0 1 1 -S ${MEM_PERCENTAGE}%  < data.corpus > model.arpa
+    python build_recommender.py --arpa model.arpa --dst s3://${BUCKET}/${CLIENT}/ngram/${START_DAY}
+fi
+
