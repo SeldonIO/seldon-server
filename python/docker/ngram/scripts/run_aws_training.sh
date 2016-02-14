b6ce1c8a5f2af5bc361b95780271ec234fa6aca1
Clive Cox <cc@seldon.io>
2016-02-14 16:46:00 +0000

    make mem percentage argument

--- python/docker/ngram/scripts/run_aws_training.sh
+++ python/docker/ngram/scripts/run_aws_training.sh
@@ -5,8 +5,8 @@ set -o errexit
 
 STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ "$#" -ne 6 ]; then
-  echo "Usage: $0 CLIENT BUCKET START_DAY DAYS AWS_KEY AWS_SECRET" >&2
+if [ "$#" -ne 7 ]; then
+  echo "Usage: $0 CLIENT BUCKET START_DAY DAYS AWS_KEY AWS_SECRET MEM_PERCENT" >&2
   exit 1
 fi
 
@@ -16,7 +16,8 @@ START_DAY=$3
 DAYS=$4
 AWS_KEY=$5
 AWS_SECRET=$6
+MEM_PERCENTAGE=$7
 
 python create_corpus.py --client ${CLIENT} --bucket ${BUCKET} --aws_key ${AWS_KEY} --aws_secret ${AWS_SECRET} --startDay ${START_DAY} --numDays ${DAYS}  --corpus data.corpus
-/kenlm/bin/lmplz -o 3 --prune 0 1 1 -S 60%  < data.corpus > model.arpa
+/kenlm/bin/lmplz -o 3 --prune 0 1 1 -S ${MEM_PERCENTAGE}%  < data.corpus > model.arpa
 python build_recommender.py --aws_key ${AWS_KEY} --aws_secret ${AWS_SECRET} --arpa model.arpa --dst s3://${BUCKET}/${CLIENT}/ngram/${START_DAY}
