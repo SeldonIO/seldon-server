218f9564c8faef578822102d33378655156c3bc1
Gurminder Sunner <gsunner2000@gmail.com>
2016-02-26 17:18:34 +0000

    DimensionsMappingManager - config loaded from zookeeper

--- server/src/io/seldon/api/locale/DimensionsMappingManager.java
+++ server/src/io/seldon/api/locale/DimensionsMappingManager.java
@@ -9,10 +9,12 @@ import java.util.Set;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.seldon.api.state.ClientConfigHandler;
 import io.seldon.api.state.ClientConfigUpdateListener;
 
 @Component
@@ -29,17 +31,31 @@ public class DimensionsMappingManager implements ClientConfigUpdateListener {
         }
     }
 
+    private final String DIMENSIONS_MAPPING_KEY = "dimensions_mapping";
     private ObjectMapper objMapper = new ObjectMapper();
     private Map<String, DimensionsMappingConfig> client_dimensions_mappings = new HashMap<>();
+
+    @Autowired
+    public DimensionsMappingManager(ClientConfigHandler configHandler) {
+        if (configHandler != null) {
+            configHandler.addListener(this);
+        }
+    }
 
     public void updateDimensionsMappingConfig(String client, String json) {
 
         try {
             DimensionsMappingConfig dimensionsMappingConfig = getDimensionsMappingConfigFromJson(json);
             client_dimensions_mappings.put(client, dimensionsMappingConfig);
+            logger.info(String.format("Updated client_dimensions_mappings for client[%s] value[%s]", client, dimensionsMappingConfig));
         } catch (Exception e) {
-            logger.error("DimensionsMappingManager failed update json config!", e);
+            logger.error("Failed update json config!", e);
         }
+    }
+
+    public void removeDimensionsMappingConfig(String client) {
+        client_dimensions_mappings.remove(client);
+        logger.info(String.format("Removed client_dimensions_mappings for client[%s]", client));
     }
 
     public Set<Integer> getMappedDimensionsByLocale(String client, Set<Integer> dimensions, String locale) {
@@ -80,12 +96,17 @@ public class DimensionsMappingManager implements ClientConfigUpdateListener {
 
     @Override
     public void configUpdated(String client, String configKey, String configValue) {
-        logger.info("configUpdated: " + client);
+        if (configKey.equals(DIMENSIONS_MAPPING_KEY)) {
+            logger.info("Received new dimensions_mapping config for " + client + ": " + configValue);
+            updateDimensionsMappingConfig(client, configValue);
+        }
     }
 
     @Override
     public void configRemoved(String client, String configKey) {
-        logger.info("configRemoved: " + client);
+        if (configKey.equals(DIMENSIONS_MAPPING_KEY)) {
+            removeDimensionsMappingConfig(client);
+        }
     }
 
 }
