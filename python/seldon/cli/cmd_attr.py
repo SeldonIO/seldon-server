846f538e9eea43bb12c9ac9bab857e37143ac6a0
Gurminder Sunner <gsunner2000@gmail.com>
2016-03-22 13:25:08 +0000

    seldon-cli - attr edit

--- python/seldon/cli/cmd_attr.py
+++ python/seldon/cli/cmd_attr.py
@@ -98,10 +98,44 @@ def action_show(command_data, opts):
     data = seldon_utils.json_to_dict(json)
     show_attr(data)
 
+def action_edit(command_data, opts):
+    client_name = opts.client_name
+    if client_name == None:
+        print "Need client name to show the attr for"
+        sys.exit(1)
+
+    zkroot = command_data["zkdetails"]["zkroot"]
+    if not is_existing_client(zkroot, client_name):
+        print "Invalid client[{client_name}]".format(**locals())
+        sys.exit(1)
+
+    zk_client = command_data["zkdetails"]["zk_client"]
+    ensure_client_has_attr(zkroot, zk_client, client_name)
+
+    data_fpath = zkroot + gdata["all_clients_node_path"] + "/" + client_name + "/attr/_data_"
+    #do the edit
+    from subprocess import call
+    editor=seldon_utils.get_editor()
+    call([editor, data_fpath])
+
+    f = open(data_fpath)
+    json = f.read()
+    f.close()
+    data = seldon_utils.json_to_dict(json)
+
+    if data is None:
+        print "Invalid attr json!"
+    else:
+        write_data_to_file(data_fpath, data)
+        node_path = gdata["all_clients_node_path"]+"/"+client_name+"/attr"
+        zk_utils.node_set(zk_client, node_path, seldon_utils.dict_to_json(data))
+        show_attr(data)
+
 def cmd_attr(command_data, command_args):
     actions = {
         "default" : action_show,
         "show" : action_show,
+        "edit" : action_edit,
     }
 
     opts = getOpts(command_args)
