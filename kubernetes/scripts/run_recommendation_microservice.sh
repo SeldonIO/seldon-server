5a7b73829b7d713b0514e32a2873e566537113e4
Clive Cox <cc@seldon.io>
2016-04-04 18:32:12 +0100

    add run microservice

---
+++ kubernetes/scripts/run_recommendation_microservice.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+if [ "$#" -ne 4 ]; then
+    echo "need <microservice_name> <microservice_image> <microservice_version> <client>"
+    exit -1
+fi
+
+STARTUP_DIR="$( cd "$( dirname "$0" )" && pwd )"
+NAME=$1
+IMAGE=$2
+VERSION=$3
+CLIENT=$4
+
+KCMD="kubectl exec seldon-control -i bash"
+
+function create_microservice_conf {
+    
+    mkdir -p ../conf/microservices
+    if test -f "${STARTUP_DIR}/../conf/microservices/microservice-${NAME}.json"; 
+    then 
+	echo "The microservice already exists. Will make a backup to .prev";
+	cp ${STARTUP_DIR}/../conf/microservices/microservice-${NAME}.json ${STARTUP_DIR}/../conf/microservices/microservice-${NAME}.json.prev
+    fi
+    cat ${STARTUP_DIR}/../conf/microservice.json.in | sed -e "s|%NAME%|${NAME}|" | sed -e "s|%IMAGE%|${IMAGE}|" | sed -e "s|%VERSION%|${VERSION}|" > ${STARTUP_DIR}/../conf/microservices/microservice-${NAME}.json
+
+}
+
+
+function run_microservice {
+
+    kubectl apply -f ${STARTUP_DIR}/../conf/microservices/microservice-${NAME}.json
+
+}
+
+function configure_seldon {
+
+    echo "seldon-cli rec_alg --action delete --client-name ${CLIENT} --recommender-name externalItemRecommendationAlgorithm" | ${KCMD}
+    echo "seldon-cli rec_alg  --action add --client-name ${CLIENT} --recommender-name externalItemRecommendationAlgorithm --config io.seldon.algorithm.external.url=http://${NAME}:5000/recommend --config io.seldon.algorithm.external.name=${NAME}" | ${KCMD}
+    echo "seldon-cli rec_alg --action commit --client-name ${CLIENT}" | ${KCMD}
+
+}
+
+
+function start_microservice {
+
+    create_microservice_conf
+
+    run_microservice
+    
+    configure_seldon
+
+}
+
+
+start_microservice "$@"
+
+
+
+
