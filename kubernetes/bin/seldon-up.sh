cdfe0c393808d16aa2196338f70314db7046f04b
Clive Cox <cc@seldon.io>
2016-04-08 09:03:57 +0100

    change seldon-up to use seldon-cli from bin folder

--- kubernetes/bin/seldon-up.sh
+++ kubernetes/bin/seldon-up.sh
@@ -37,13 +37,13 @@ function start_api_server {
 function setup_basic_conf {
 
     # Setup memcached
-    echo "seldon-cli memcached --action setup --servers memcached1:11211,memcached2:11211" | ${KCMD}
-    echo "seldon-cli memcached --action commit" | ${KCMD}
+    ${STARTUP_DIR}/seldon-cli memcached --action setup --servers memcached1:11211,memcached2:11211
+    ${STARTUP_DIR}/seldon-cli memcached --action commit
     # Setup DB
-    echo "seldon-cli db --action setup --db-name ClientDB --db-jdbc 'jdbc:mysql:replication://mysql:3306,mysql:3306/?characterEncoding=utf8&useServerPrepStmts=true&logger=com.mysql.jdbc.log.StandardLogger&roundRobinLoadBalance=true&transformedBitIsBoolean=true&rewriteBatchedStatements=true'" | ${KCMD}
+    ${STARTUP_DIR}/seldon-cli db --action setup --db-name ClientDB --db-jdbc 'jdbc:mysql:replication://mysql:3306,mysql:3306/?characterEncoding=utf8&useServerPrepStmts=true&logger=com.mysql.jdbc.log.StandardLogger&roundRobinLoadBalance=true&transformedBitIsBoolean=true&rewriteBatchedStatements=true'
     echo "seldon-cli db --action commit" | ${KCMD}
     # Setup Test client
-    echo "seldon-cli client --action setup --db-name ClientDB --client-name test" | ${KCMD}
+    ${STARTUP_DIR}/seldon-cli client --action setup --db-name ClientDB --client-name test
 
 }
 
