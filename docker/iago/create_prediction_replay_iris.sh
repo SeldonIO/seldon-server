a72ab8a78f529fca48cb85474e51c75089f6c5c1
Clive Cox <cc@seldon.io>
2016-06-09 17:54:37 +0100

    change iago prediction replay create script

--- docker/iago/create_prediction_replay_iris.sh
+++ docker/iago/create_prediction_replay_iris.sh
@@ -3,13 +3,15 @@
 set -o nounset
 set -o errexit
 
-if [ "$#" -ne 2 ]; then
-    echo "need <client name> <file>"
+if [ "$#" -ne 4 ]; then
+    echo "need <client name> <file> <num_api_calls> <endpoint>"
     exit -1
 fi
 
 CLIENT=$1
 FILE=$2
+NUM_API=$3
+ENDPOINT=$4
 
 seldon-cli -q keys --client-name ${CLIENT} --scope js > key.json
-python create_prediction_replay.py --key key.json --replay ${FILE}  --feature '{"name":"f1","type":"numeric","min":0,"max":5}' --feature '{"name":"f2","type":"numeric","min":0,"max":5}' --feature '{"name":"f3","type":"numeric","min":0,"max":5}' --feature '{"name":"f4","type":"numeric","min":0,"max":5}'
+python create_prediction_replay.py --key key.json --replay ${FILE}  --feature '{"name":"f1","type":"numeric","min":0,"max":5}' --feature '{"name":"f2","type":"numeric","min":0,"max":5}' --feature '{"name":"f3","type":"numeric","min":0,"max":5}' --feature '{"name":"f4","type":"numeric","min":0,"max":5}' --num ${NUM_API} --endpoint ${ENDPOINT}
