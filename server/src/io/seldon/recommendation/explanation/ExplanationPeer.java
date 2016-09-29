fa7969365ea0d34af45804ed2154bf904373f555
Gurminder Sunner <gsunner2000@gmail.com>
2016-09-29 13:49:35 +0100

    default recommendation explanation updated

--- server/src/io/seldon/recommendation/explanation/ExplanationPeer.java
+++ server/src/io/seldon/recommendation/explanation/ExplanationPeer.java
@@ -68,7 +68,7 @@ public class ExplanationPeer implements ClientConfigUpdateListener {
 
     private Map<String, RecommendationExplanationConfig> client_recommendation_explanation_configs = new ConcurrentHashMap<>();
 
-    private final static String DEFAULT_EXPLANATION = "recommended due to your recent activity";
+    private final static String DEFAULT_EXPLANATION = "recommended based on your recent activity";
 
     @Autowired
     public ExplanationPeer(ExceptionSwallowingMemcachedClient memcacheClient, ClientConfigHandler configHandler) {
