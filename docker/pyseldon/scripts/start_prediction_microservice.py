271a455b810090b869d2199d74b5e178e82a309a
Clive Cox <cc@seldon.io>
2016-10-05 18:17:42 +0100

    revert to bask Flask web app

--- docker/pyseldon/scripts/start_prediction_microservice.py
+++ docker/pyseldon/scripts/start_prediction_microservice.py
@@ -20,7 +20,5 @@ if __name__ == "__main__":
     m = Microservices(aws_key=args.aws_key,aws_secret=args.aws_secret)
     app = m.create_prediction_microservice(args.pipeline,args.model_name)
 
-    from gevent.wsgi import WSGIServer
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    app.run(host="0.0.0.0", debug=True)
 
--- docker/pyseldon/scripts/start_recommendation_microservice.py
+++ docker/pyseldon/scripts/start_recommendation_microservice.py
@@ -15,7 +15,5 @@ if __name__ == "__main__":
     m = Microservices(aws_key=args.aws_key,aws_secret=args.aws_secret)
     app = m.create_recommendation_microservice(args.recommender,memcache_servers=args.memcache_servers,memcache_pool_size=args.memcache_pool_size)
 
-    from gevent.wsgi import WSGIServer
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    app.run(host="0.0.0.0", debug=True)
 
