a9d23cce1e21f1b70232f117972a9dc5e3ae90b8
Gurminder Sunner <gsunner2000@gmail.com>
2016-10-27 12:19:58 +0100

    seldon-cli keys - added action update

--- python/seldon/cli/db_utils.py
+++ python/seldon/cli/db_utils.py
@@ -30,5 +30,22 @@ def get_keys(dbSettings,client_name,scope):
     res = []
     for row in rows:
         res.append({"db":dbSettings["name"],"client":row[0],"key":row[1],"secret":row[2],"scope":row[3]})
-    cur.close()    
+    cur.close()
     return res
+
+def set_keys(dbSettings,client_name,scope,consumer_key, consumer_secret):
+    db = MySQLdb.connect(host=dbSettings["host"],
+                         user=dbSettings["user"],
+                         passwd=dbSettings["password"])
+    cur = db.cursor()
+    UPDATE_KEYS_SQL_SCOPE_ALL="update api.consumer set consumer_key=%(consumer_key)s,consumer_secret=%(consumer_secret)s where short_name=%(client_name)s and scope=%(scope)s"
+    UPDATE_KEYS_SQL_SCOPE_JS="update api.consumer set consumer_key=%(consumer_key)s where short_name=%(client_name)s and scope=%(scope)s"
+    sql = UPDATE_KEYS_SQL_SCOPE_JS if scope == 'js' else UPDATE_KEYS_SQL_SCOPE_ALL
+    cur.execute(sql,{
+        "client_name":client_name,
+        "scope":scope,
+        "consumer_key":consumer_key,
+        "consumer_secret":consumer_secret,
+    })
+    cur.fetchall()
+
