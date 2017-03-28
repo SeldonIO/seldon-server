c7f913a924fd3b332e48b9cee426f6c739a52bc3
giovannivacanti <giova.physics@gmail.com>
2017-03-28 12:41:48 +0100

    logger level set to INFO

--- python/seldon/anomaly/AnomalyDetection.py
+++ python/seldon/anomaly/AnomalyDetection.py
@@ -8,7 +8,7 @@ from time import time
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
