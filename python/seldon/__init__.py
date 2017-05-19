0d4c979d8a874d7ba38d67761575a8562f90044f
Clive Cox <cc@seldon.io>
2017-05-19 14:09:57 +0100

    update python version number

--- python/seldon/__init__.py
+++ python/seldon/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from .util import RecommenderWrapper,Recommender,Extension,ExtensionWrapper,Recommender_wrapper,Extension_wrapper
 
-__version__ = '2.2.2'
+__version__ = '2.2.3'
 
 class NullHandler(logging.Handler):
     def emit(self, record):
